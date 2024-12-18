5fe18195bf5d50a2ee1dfaa0242eeaf2e56ca2f4
oweeeenie <owensullivan419@yahoo.com>
2024-12-17 20:33:12 -0500

    added primary functionality for the knights movement

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // all logic
-function knightMoves(start, end) {}
+function knightMoves(start, end) {
+  let starting = start;
+  let queue = [];
+  let moves = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+  ];
+
+  if (start === end) {
+    return;
+  }
+
+  for (let i = 0; i < moves.length; i++) {
+    let newX = starting[0] + moves[i][0];
+    let newY = starting[1] + moves[i][1];
+
+    if (newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7) {
+      queue.push([newX, newY]);
+    }
+  }
+}
